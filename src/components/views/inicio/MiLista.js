26a69e6922464f489d7b9eef1338fd6bc0400dd3
Juan Jesus ML <juanjesusmatias06@gmail.com>
2021-03-01 18:04:28 -0500

    componente de carrusel de pelicula

--- src/components/views/inicio/MiLista.js
+++ src/components/views/inicio/MiLista.js
@@ -7,7 +7,7 @@ const MiLista = () => {
             <h1>Mi lista</h1>
             <div className="wrapper">
                 <section id="section1">
-                    {/* <a href="#section2" className="arrow__btn izquierda">›</a> */}
+                    {/* <a href="#section2" className="izquierda">›</a> */}
                     <div className="item">
                         <img src="https://occ-0-1567-1123.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABRvngexxF8H1-OzRWFSj6ddD-aB93tTBP9kMNz3cIVfuIfLEP1E_0saiNAwOtrM6xSOXvoiSCMsihWSkW0dq808-R7_lBnr6WHbjkKBX6I3sD0uCcS8kSPbRjEDdG8CeeVXEAEV6spQ.webp" alt="Describe Image" />
                     </div>
@@ -23,11 +23,11 @@ const MiLista = () => {
                     <div className="item">
                         <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABbcCX42tsqGbBvO2y9CQv5-7QvYbCfoHtXsuc6NPCtZaKa4l4fBX3XWvUwG9F2A3CTsNpHVmulxBbdXKwK8Q6xGjejd9FoadGkZ7CnGrSl601TOQjzSHJ23NuIPC8j0QMGORL4uRIA.jpg" alt="Describe Image" />
                     </div>
-                    <a href="#section2" className="arrow__btn derecha">›</a>
+                    <a href="#section2" className="derecha">›</a>
                 </section>
                 
                 <section id="section2">
-                    <a href="#section1" className="arrow__btn izquierda">‹</a>
+                    <a href="#section1" className="izquierda">‹</a>
                     <div className="item">
                         <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABVopDZ5Fy9_fk_HO5WxHTXKKjKhtWFupbSjuvPwfLK_vytzon4EwRUdGgYJ34JwPxOTK_NkV3aXfkULMB0Dcct-FyfqzH-X44VXuRMp4QeBHlvKwWeZFpZlGdItPzmmg4scmwhG7SQ.jpg" alt="Describe Image" />
                     </div>
@@ -43,11 +43,11 @@ const MiLista = () => {
                     <div className="item">
                         <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABbXWODpAWqVXcmmjMA7K-2mPkQpvwCLfSdeyhVXzR8A3MSpdSEnnjf4HEJJTYC-TnktU6njTUGAxmzWEYCaJbk4v_ZeL-7QGzmkvYBjg_N-evr2XmcX-Fanoyvu_nimFP4iigPe4O3Vr_WcgplhwkDrJwPUJa84wRLrNAx3TufN5V7cWRP4indqu5HQahvgFEqfL9zjp4g.jpg" alt="Describe Image" />
                     </div>
-                    <a href="#section3" className="arrow__btn  derecha">›</a>
+                    <a href="#section3" className="derecha">›</a>
                 </section>
                 
                 <section id="section3">
-                    <a href="#section2" className="arrow__btn izquierda">‹</a>
+                    <a href="#section2" className="izquierda">‹</a>
                     <div className="item">
                         <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABRr4YxdaABuAuH_3FmSQZn7BCvLp-UUPsXE9MiYpvFP3CSUHV2zOew5oVqKqqdaOd3tbFVS0Uf67uIs7_eZydlCghg4nK0nMatRpPImABwTOhnNzCLCxdKrua7pPIcPCZqBYTeAO5g.jpg" alt="Describe Image" />
                     </div>
@@ -63,7 +63,7 @@ const MiLista = () => {
                     <div className="item">
                         <img src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABVxuRB932hvre-XP0gh6ar5ztoR3Oe3QjKHkyvcDnRak2MKXOrx5H7mFQSvggefMFOppwEs7ZCCpiqrJ_CYGvtvYB9NpU4SWUtNO6uV2u-DTID267AcHjHcGvGBQJ1ufddDkxcGOZyi5MlOQ5QUmBun4652FbYUnib3zMYQDgcna_Pvz8y_HO5fbokxezrRR1JZAAiqFSQ.jpg" alt="Describe Image" />
                     </div>
-                    {/* <a href="#section1" className="arrow__btn derecha">›</a> */}
+                    {/* <a href="#section1" className=" derecha">›</a> */}
                 </section>
             </div>
         </>
