a679d956b1f90aee112258f10d6bd01c44c564bc
Giancarlo <matiasmauriciogiancarlo@gmail.com>
2021-03-11 07:33:24 -0500

    updated

--- src/components/layout/WrapperCarrusel.js
+++ src/components/layout/WrapperCarrusel.js
@@ -2,11 +2,12 @@ import React from 'react'
 import PortadaPelicula from '../views/inicio/PortadaPelicula'
 import PeliculasPopulares from '../views/novedades-populares/PeliculasPopulares'
 import NovedadesPopulares from '../views/NovedadesPopulares'
+import './styles/wrapper-carrusel.css'
 
 const WrapperCarrusel = (props) => {
     return (
         <>
-            <h1></h1>
+            <h1>Peliculas</h1>
             <div className="wrapper">
                 <section>
                     {props.children}
